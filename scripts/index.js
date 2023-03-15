53d18dc491fe5faa012ba691f3d9a3500b4a9bd1
SoldierMedic <pv2rojas@gmail.com>
2023-03-14 20:58:35 -0400

    added comments

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,25 @@ const statusDisplay = document.getElementById('statusDisplay')
 
 const numbers = [val1,val2,val3]
 
-const performArithmetics = () => {
-    const [firstNum, secondNum, thirdNum] = numbers.map(val => parseInt(val.value))
+const performArithmetics = () => {//anonymous function for readability
+    const [firstNum, secondNum, thirdNum] = numbers.map(val => parseInt(val.value)) //creates an array of integers
     
-    if (isNaN(firstNum) || isNaN(secondNum) || isNaN(thirdNum)) {
+    if (isNaN(firstNum) || isNaN(secondNum) || isNaN(thirdNum)) {//checks number validity
         statusDisplay.innerHTML = 'Please enter valid numbers.'
         return
     }
     
     const sortedNums = [firstNum, secondNum, thirdNum].sort((a, b) => a - b)
     const [low, median, high] = sortedNums
-    const average = (sortedNums.reduce((acc, curr) => acc + curr, 0) / sortedNums.length).toFixed(2)
+    const average = (sortedNums.reduce((acc, curr) => acc + curr, 0) / sortedNums.length).toFixed(2) //computes average fixed to 2 decimal places
     const range = high - low
     
     updateDisplay(high, low, median, average, range)
 }
 
-const updateDisplay = (high, low, median, average, range) => {
+const updateDisplay = (high, low, median, average, range) => {//anonymous function for readability.
     statusDisplay.innerHTML = `
-        High: ${high}, 
+        High: ${high}, //template literal syntax
         Low: ${low}, 
         Median: ${median}, 
         Average: ${average}, 
